810423d136c3841532feb257574562c8402b897a
Pailavyshnavi30741 <2400030741@kluniversity.in>
2026-09-18 11:43:37 +0530

    Add SOA SDP microservices project

---
+++ BookingService/src/test/java/com/klef/ms/BookingService/BookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klef.ms.BookingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ EventService/src/main/java/com/klef/soa/sdp/EventServiceApplication.java
@@ -0,0 +1,5 @@
+package com.klef.soa.sdp;
+
+public class EventServiceApplication {
+
+}
